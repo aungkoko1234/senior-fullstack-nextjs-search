f0d8c81749d641e2e28fbd0f28e4fe617b079668
Aung Ko Ko <aungkoko@zig.live>
2022-08-25 00:08:06 +0630

    initial set up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr'
 
 export default function App({ Component, pageProps }: AppProps) {
   const AnyComponent = Component as any
-  return <AnyComponent {...pageProps} />
+  return (
+    <SWRConfig value={{ onError: (error: Error) => console.log(error) }}>
+      <AnyComponent {...pageProps} />
+    </SWRConfig>
+  )
 }
